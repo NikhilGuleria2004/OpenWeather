94e751fd77914577590da42d9e2dfac962f2386b
NikhilGuleria2004 <163378911+NikhilGuleria2004@users.noreply.github.com>
2024-10-04 14:42:30 +0530

    Update vite.config.js

--- weather-app-frontend/vite.config.js
+++ weather-app-frontend/vite.config.js
@@ -4,9 +4,10 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist'
+    outDir: 'dist', // Directory where the build files will be output
   },
   server: {
+    // Proxy API calls to avoid CORS issues in development
     proxy: {
       '/api': 'https://openweatherappnikhil.vercel.app/'
     }
